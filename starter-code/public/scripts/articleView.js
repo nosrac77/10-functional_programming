3986b5dca76e41300834965431665a86b093c39e
nosrac77 <carson.newton@outlook.com>
2017-08-26 10:07:45 -0700

    worked on some TODOs

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -1,6 +1,7 @@
 'use strict';
 var app = app || {};
 
+(function(module) {
 // TODO: Wrap the contents of this file, except for the preceding 'use strict' and 'var app...' declararions, in an IIFE.
 // Give the IIFE a parameter called 'module'.
 // At the very end of the code, but still inside the IIFE, attach the 'articleView' object to 'module'.
@@ -138,12 +139,17 @@ articleView.initAdminPage = function() {
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
   // we are then calling "template" on line 117.
 
+  let statData = $('#author-stats-template').html();
+  let template = Handlebars.compile(statData);
+
   // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
   // appending to the DOM.
   // The callback is not required to return anything.
-  app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
+  //app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
   // REVIEW: Simply write the correct values to the page:
   $('#blog-stats .articles').text(app.Article.all.length);
   $('#blog-stats .words').text(app.Article.numWordsAll());
 };
+module.articleView = articleView;
+})(app);
--- starter-code/server.js
+++ starter-code/server.js
@@ -7,8 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = 'postgres://postgres:Skrillexfan7@localhost:5432/lab10'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(err) {
